438f0ae50f58baffacbfbe5dea903641992c7ba5
sensecbc <rtavares4@ua.pt>
2024-08-02 11:13:20 +0100

    added button that made changing grid possible

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = `${gridSize}px`; //600px comprimento
 sketchArea.style.height = `${gridSize}px`; //600px altura
 
-function createGridCells(value) {
+function createGridCells() {
   //funcao para criar grid
   for (i = 0; i < cols * rows; i++) {
     //cols * rows pq vai ser o nr de quadrados q precisamos para preencher um 16x16 so com quadrados perfeitos
     const gridCell = document.createElement("div"); //cria a div
     gridCell.style.width = `${gridSize / cols - 2}px`; // 600 / 16 = 37.5 q e o comprimento perfeito para o quadrado e aqui estamos a dar set ao width
     gridCell.style.height = `${gridSize / rows - 2}px`; // mesma coisa mas para a altura
-    gridCell.classList.add("cell"); //adiciona uma class para alterarmos o cs
+    gridCell.classList.add("cell"); //adiciona uma class para alterarmos o css
     sketchArea.appendChild(gridCell); //adiciona o gridcell ao container sketcharea
 
     gridCell.addEventListener("mouseover", blackColor); //mouse hover
@@ -23,16 +23,33 @@ function createGridCells(value) {
     }
   }
 }
-
 createGridCells();
 
 //gridCell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; rgb coloring
 
-resizeButton.addEventListener("click", resize);
+resizeButton.addEventListener("click", changeSize);
+function changeSize() {
+  let promptUser = Number(prompt("type a number from 1 to 100"));
 
-function resize() {
-  let promptUser = prompt(
-    "Resize the paint area by telling us the new x and y"
-  );
-  createGridCells(promptUser);
+  if (promptUser > 100) {
+    let prompt2 = prompt("I SAID A NUMBER FROM 1 TO 100");
+  } else if (promptUser < 101) {
+    sketchArea.innerHTML = ""; //apaga o conteudo do sketcharea. da clear ao html
+    createGridCellsTwo(promptUser);
+  }
+
+  function createGridCellsTwo() {
+    for (i = 0; i < promptUser * promptUser; i++) {
+      const gridCell = document.createElement("div"); //cria a div
+      gridCell.style.width = `${gridSize / promptUser - 2}px`; // 600 / 16 = 37.5 q e o comprimento perfeito para o quadrado e aqui estamos a dar set ao width
+      gridCell.style.height = `${gridSize / promptUser - 2}px`; // mesma coisa mas para a altura
+      gridCell.classList.add("cell"); //adiciona uma class para alterarmos o css
+      sketchArea.appendChild(gridCell); //adiciona o gridcell ao container sketcharea
+
+      gridCell.addEventListener("mouseover", blackColor); //mouse hover
+      function blackColor() {
+        gridCell.style.backgroundColor = "black"; //muda a cor de cada quadrado p preto
+      }
+    }
+  }
 }
